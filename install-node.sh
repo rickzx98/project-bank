985e90bc1ca89347dbb4a99a82cbe179071aad95
Jerico <rickzx98@gmail.com>
2018-07-06 23:03:36 +0800

    updates

--- install-node.sh
+++ install-node.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sh export-env.sh;
+export CORDA_NODE=Notary;
+export CORDA_NOTARY_IP=54.190.42.1;
+export CORDA_LONDON_IP=18.237.44.123;
+export CORDA_NEW_YORK_IP=34.217.122.211;
+export CORDA_PARIS_IP=18.237.35.221;
 cordaHome=/opt/corda;
 mkdir $cordaHome;
 sh gradlew deployNodesProd;
