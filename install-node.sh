bc711f3a1125db55311361110e6af44b097bf547
Jerico <rickzx98@gmail.com>
2018-07-06 14:23:39 +0800

    updates

---
+++ install-node.sh
@@ -0,0 +1,46 @@
+mkdir /opt/Corda
+
+sh gradlew deployNodesProd
+cp  -i -R -y ./java-source/build/node/$CORDA_NODE/*  /opt/corda/*
+
+echo `
+    [Unit]
+    Description=Corda Node - $CORD_NODE
+    Requires=network.target
+
+    [Service]
+    Type=simple
+    User=corda
+    WorkingDirectory=/opt/corda
+    ExecStart=/usr/bin/java -Xmx2048m -jar /opt/corda/corda.jar
+    Restart=on-failure
+
+    [Install]
+    WantedBy=multi-user.target` >  /etc/systemd/system/corda.service
+echo `
+    description "Corda Node - $CORDA_NODE"
+
+    start on runlevel [2345]
+    stop on runlevel [!2345]
+
+    respawn
+    setuid corda
+    chdir /opt/corda
+    exec java -Xmx2048m -jar /opt/corda/corda.jar` >  /etc/init/corda.conf
+
+
+
+if [$CORDA_NODE != 'Notary']
+then
+    echo `
+        description "Webserver for Corda Node - $CORDA_NODE"
+
+        start on runlevel [2345]
+        stop on runlevel [!2345]
+
+        respawn
+        setuid corda
+        chdir /opt/corda
+        exec java -jar /opt/corda/corda-webserver.jar
+    `
+fi 
