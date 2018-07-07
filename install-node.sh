f91a8c7a3d58c1bcec5e98a85f12fcfc1e5ad8a4
Jerico <rickzx98@gmail.com>
2018-07-08 02:33:59 +0800

    updates

--- install-node.sh
+++ install-node.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 export CORDA_NODE=London;
-export CORDA_NOTARY_IP=54.190.42.1;
+#export CORDA_NOTARY_IP=54.190.42.1;
 #export CORDA_LONDON_IP=18.237.44.123;
-export CORDA_LONDON_IP=localhost;
-export CORDA_NEW_YORK_IP=34.217.122.211;
-export CORDA_PARIS_IP=18.237.35.221;
+#export CORDA_NEW_YORK_IP=34.217.122.211;
+#export CORDA_PARIS_IP=18.237.35.221;
 echo Setting up $CORDA_NODE;
 cordaHome=/opt/corda;
 mkdir $cordaHome;
