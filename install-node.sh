23bc72d64936da93668489503d345d6f349f41f3
Jerico <rickzx98@gmail.com>
2018-07-07 22:46:18 +0800

    updates

--- install-node.sh
+++ install-node.sh
@@ -3,16 +3,7 @@ export CORDA_NOTARY_IP=54.190.42.1;
 export CORDA_LONDON_IP=18.237.44.123;
 export CORDA_NEW_YORK_IP=34.217.122.211;
 export CORDA_PARIS_IP=18.237.35.221;
-
-if [ $CORDA_NODE = "Notary" ]; then
-export CORDA_NOTARY_IP=localhost;
-elif [ $CORDA_NODE = "London" ]; then
-export CORDA_LONDON_IP=localhost
-elif [ $CORDA_NODE = "NewYork" ]; then
-export CORDA_NEW_YORK_IP=localhost
-elif [ $CORDA_NODE = "Paris" ]; then
-export CORDA_PARIS_IP=localhost;
-fi
+if [ $CORDA_NODE = "Notary" ]; then export CORDA_NOTARY_IP=localhost; elif [ $CORDA_NODE = "London" ]; then export CORDA_LONDON_IP=localhost; elif [ $CORDA_NODE = "NewYork" ]; then export CORDA_NEW_YORK_IP=localhost; elif [ $CORDA_NODE = "Paris" ]; then export CORDA_PARIS_IP=localhost; fi
 cordaHome=/opt/corda;
 mkdir $cordaHome;
 sh gradlew deployNodesProd;
@@ -45,8 +36,7 @@ echo "
     chdir $cordaHome
     exec java -Xmx2048m -jar $cordaHome/corda.jar" >> /etc/init/corda.conf;
 sudo rm /etc/init/corda-webserver.conf;
-if [ $CORDA_HOME != "Notary"]; then
-echo "
+if [ $CORDA_HOME != "Notary"]; then echo "
         description \"Webserver for Corda Node - $CORDA_NODE\"
 
         start on runlevel [2345]
@@ -56,5 +46,4 @@ echo "
         setuid corda
         chdir $cordaHome
         exec java -jar $cordaHome/corda-webserver.jar
-    " >> /etc/init/corda-webserver.conf;
-fi
+    " >> /etc/init/corda-webserver.conf; fi
