b6bc4c2f65d9ec8ec579b90e9e5453fe82b35298
Jerico <rickzx98@gmail.com>
2018-07-07 20:47:52 +0800

    updates

--- install-node.sh
+++ install-node.sh
@@ -4,23 +4,13 @@ export CORDA_LONDON_IP=18.237.44.123;
 export CORDA_NEW_YORK_IP=34.217.122.211;
 export CORDA_PARIS_IP=18.237.35.221;
 
-if [ "$CORDA_NODE" == "Notary" ] ;
-then
+if [ "$CORDA_NODE" == "Notary" ]; then
 export CORDA_NOTARY_IP=localhost
-fi
-
-if [ "$CORDA_NODE" == "London" ] ;
-then
+elif [ "$CORDA_NODE" == "London" ]; then
 export CORDA_LONDON_IP=localhost
-fi
-
-if [ "$CORDA_NODE" == "NewYork" ] ;
-then
+elif [ "$CORDA_NODE" == "NewYork" ]; then
 export CORDA_NEW_YORK_IP=localhost
-fi
-
-if [ "$CORDA_NODE" == "Paris" ] ;
-then
+elif [ "$CORDA_NODE" == "Paris" ]; then
 export CORDA_PARIS_IP=localhost
 fi
 
@@ -56,8 +46,7 @@ echo "
     chdir $cordaHome
     exec java -Xmx2048m -jar $cordaHome/corda.jar" >> /etc/init/corda.conf;
 sudo rm /etc/init/corda-webserver.conf;
-if [ "$CORDA_HOME" != "Notary"] ;
-then
+if [ "$CORDA_HOME" != "Notary"]; then
 echo "
         description \"Webserver for Corda Node - $CORDA_NODE\"
 
