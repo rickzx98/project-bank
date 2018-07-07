38af59dd02ac2c08f463194b0a43a2cf31d584b8
Ubuntu <ubuntu@ip-172-31-3-209.us-west-2.compute.internal>
2018-07-07 18:09:30 +0000

    updates

--- install-node.sh
+++ install-node.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+export CORDA_NODE=London;
 export CORDA_NOTARY_IP=54.190.42.1;
-export CORDA_LONDON_IP=18.237.44.123;
+#export CORDA_LONDON_IP=18.237.44.123;
+export CORDA_LONDON=localhost;
 export CORDA_NEW_YORK_IP=34.217.122.211;
 export CORDA_PARIS_IP=18.237.35.221;
-if [ $CORDA_NODE == "Notary" ]; then export CORDA_NOTARY_IP=localhost; elif [ $CORDA_NODE == "London" ]; then export CORDA_LONDON_IP=localhost; elif [ $CORDA_NODE == "NewYork" ]; then export CORDA_NEW_YORK_IP=localhost; elif [ $CORDA_NODE == "Paris" ]; then export CORDA_PARIS_IP=localhost; fi
+echo Setting up $CORDA_NODE;
 cordaHome=/opt/corda;
 mkdir $cordaHome;
-sh gradlew deployNodesProd;
+sudo sh gradlew deployNodesProd;
 sudo cp -R ./java-source/build/nodes/$CORDA_NODE/* $cordaHome/;
 sudo rm /etc/systemd/system/corda.service;
 sudo rm /etc/init/corda.conf;
-echo "Setting up $CORDA_NODE";
 echo "
     [Unit]
     Description=Corda Node - $CORD_NODE
@@ -25,25 +26,6 @@ echo "
 
     [Install]
     WantedBy=multi-user.target" >> /etc/systemd/system/corda.service;
-echo "
-    description \"Corda Node - $CORDA_NODE\"
+sudo chown ubuntu:root /etc/systemd/system/corda.service;
+sudo chmod 644 /etc/systemd/system/corda.service;
 
-    start on runlevel [2345]
-    stop on runlevel [!2345]
-
-    respawn
-    setuid corda
-    chdir $cordaHome
-    exec java -Xmx2048m -jar $cordaHome/corda.jar" >> /etc/init/corda.conf;
-sudo rm /etc/init/corda-webserver.conf;
-if [ $CORDA_HOME != "Notary"]; then echo "
-        description \"Webserver for Corda Node - $CORDA_NODE\"
-
-        start on runlevel [2345]
-        stop on runlevel [!2345]
-
-        respawn
-        setuid corda
-        chdir $cordaHome
-        exec java -jar $cordaHome/corda-webserver.jar
-    " >> /etc/init/corda-webserver.conf; fi
