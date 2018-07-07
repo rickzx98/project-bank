4cccfecb94eaa1c5d392c80923d5c5d2cf5aec6d
Jerico <rickzx98@gmail.com>
2018-07-08 02:12:29 +0800

    updates

--- install-node.sh
+++ install-node.sh
@@ -12,7 +12,7 @@ sudo sh gradlew deployNodesProd;
 sudo cp -R ./java-source/build/nodes/$CORDA_NODE/* $cordaHome/;
 sudo rm /etc/systemd/system/corda.service;
 sudo rm /etc/init/corda.conf;
-echo "
+sudo echo "
     [Unit]
     Description=Corda Node - $CORD_NODE
     Requires=network.target
@@ -28,4 +28,22 @@ echo "
     WantedBy=multi-user.target" >> /etc/systemd/system/corda.service;
 sudo chown ubuntu:root /etc/systemd/system/corda.service;
 sudo chmod 644 /etc/systemd/system/corda.service;
+sudo echo "
+    [Unit]
+    Description=Webserver for Corda Node - Bank of Breakfast Tea
+    Requires=network.target
 
+    [Service]
+    Type=simple
+    User=corda
+    WorkingDirectory=/opt/corda
+    ExecStart=/usr/bin/java -jar /opt/corda/corda-webserver.jar
+    Restart=on-failure
+
+    [Install]
+    WantedBy=multi-user.target" >> /etc/systemd/system/corda-webserver.service;
+sudo chown ubuntu:root /etc/systemd/system/corda-webserver.service;
+sudo chmod 644 /etc/systemd/system/corda-webserver.service;
+sudo systemctl daemon-reload;
+sudo systemctl enable --now corda;
+sudo systemctl enable --now corda-webserver;
